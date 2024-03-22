a45630555ff3c59a8f7c4354a8e909b911761bb2
taradelic <153901162+taradelic@users.noreply.github.com>
2024-03-22 09:22:01 +0100

    Add files via upload

---
+++ pzw_projekt/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/")
+def osnovno():
+    return render_template("osnovno.html")
+
+@app.route("/")
+def phyton():
+    return render_template("phyton.html")
+
+@app.route("/")
+def flask():
+    return render_template("flask.html")
+
+
+if __name__ == "__main__":
+    app.run()
